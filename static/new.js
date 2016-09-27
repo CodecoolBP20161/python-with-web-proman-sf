b106e0f9cb86f66bf24a017bc50744d637c8f911
balag3 <carbalage@gmail.com>
2016-09-27 15:00:56 +0200

    Adds popup to create boards.

--- static/new.js
+++ static/new.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
+    $(".popup").hide();
+
     $("#btn").click(function(){
-        $("#popup").dialog();
+        $(".popup").dialog();
+    });
+    $("#save").click(function(){
+        var $board_title = $(".title").val()
+        $(".board").append("<p class=\"text-center\">" + $board_title + "</p>")
+         $('.title').val("");
+        $(".popup").dialog('close');
     });
 });
