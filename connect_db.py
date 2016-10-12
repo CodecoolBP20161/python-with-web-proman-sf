e8477524fd3219e08f32ca4196d961795ec9d269
balag3 <carbalage@gmail.com>
2016-10-12 15:16:08 +0200

    Adds database singleton

--- connect_db.py
+++ connect_db.py
@@ -1,3 +1,27 @@
-from peewee import *
+from peewee import PostgresqlDatabase
+from config import Config
 
-db = PostgresqlDatabase('', user='', password='')
+
+def singleton(cls):
+    instances = {}
+
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
+
+
+@singleton
+class DataBase():
+
+    def __init__(self):
+        self.database = Config.load("database")
+        self.user = Config.load("database_user")
+        self.password = Config.load("database_password")
+
+    def connect(self):
+        return PostgresqlDatabase(self.database, user=self.user, password=self.password)
+
+my_database = DataBase()
+db = my_databse.connect()
